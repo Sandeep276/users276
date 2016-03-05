025070d0260b1cd17f223f8cbbde630556133ab5
Sandeep276 <sandeepaws660@gmail.com>
2016-03-05 17:28:02 +0530

    Create Providers.rb

---
+++ Providers.rb
@@ -0,0 +1,11 @@
+user 'sandeep' do
+  supports :Chef_AWS => true
+  comment 'S/W'
+  uid '1234'
+  gid '1234'
+  home '/home/Sandeep'
+  shell '/bin/bash'
+  password '$1$JJsvHslV$szsCjVEroftprNn4JHtDi'
+end
+
+
